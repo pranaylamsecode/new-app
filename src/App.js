6fc80271b3c0048ff3d5e7f73aa13cdd5dd8b9b9
pranaylamsecode <pranaylamsecode@gmail.com>
2023-01-16 12:37:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./pages/Footer";
+import Header from "./pages/Header";
+import Hero from "./pages/Hero";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
