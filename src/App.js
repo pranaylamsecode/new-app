2cf53598169aece9e1f49b96e4c2885ba3671450
pranaylamsecode <pranaylamsecode@gmail.com>
2023-02-16 13:28:04 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import CssTransition from "./pages/css-transition/CssTransition";
+import OutIn from "./pages/out-in-page/OutIn";
+import PhotoCollage from "./pages/PhotoCollage";
 
 import ReactRouterAnimation from "./pages/react-router-animation/ReactRouterAnimation";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <ReactRouterAnimation />
-      </BrowserRouter>
+      <PhotoCollage />
     </>
   );
 };
